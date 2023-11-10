1e88f0a116717115cb567e27e2a2c427811867e6
DanHakGit <danielhakimian@proton.me>
2023-11-10 14:40:40 -0500

    New level and many fixes

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -10,34 +10,29 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var wall_pushing = false
 var was_wall_jumping = false
 
-#Player Functions
+# Player Functions
 func _physics_process(delta):
 
-	#Allows player to drop through the platforms
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if Input.is_action_pressed("Down") and collision.get_collider().is_in_group("OneWayCollisions"):
-			position.y += 1
-		
-	# Add the gravity.
+	# Assign values to left and right
+	var direction = Input.get_axis("Left", "Right")
+	
+	# Makes gravity take effect
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Allows player to jump
-	if Input.is_action_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Quits game
-	if Input.is_action_just_pressed("Quit"):
-		get_tree().quit()
 
 	# Get the input direction and handle the movement/deceleration
-	# As good practice, you should replace UI actions with custom gameplay actions
-	var direction = Input.get_axis("Left", "Right")
 	if direction and not wall_pushing:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Allows player to drop through one way platforms
+	if Input.is_action_pressed("Down") and $RayDown.is_colliding() and $RayDown.get_collider().is_in_group("OneWayCollisions") and direction == 0:
+		position.y += 1
 	
 	# Allows the player to wall slide (some parts taken from https://www.youtube.com/watch?v=5FWzWrK6jLM)
 	var is_wall_sliding = false
@@ -45,17 +40,15 @@ func _physics_process(delta):
 	if is_on_wall_only() and velocity.y > 0:
 		if Input.is_action_pressed("Left") and $RayLeft.is_colliding():
 			is_wall_sliding = true
-			$Sprite2D.flip_h = true
-			
+	
 		elif Input.is_action_pressed("Right") and $RayRight.is_colliding():
 			is_wall_sliding = true
-			$Sprite2D.flip_h = false
 	
 	if is_wall_sliding:
 		velocity.y += WALL_GRAVITY
 		velocity.y = min(velocity.y, WALL_GRAVITY)
 		$Sprite2D.animation = "Sliding"
-
+	
 	# Allows the player to wall jump
 	if Input.is_action_just_pressed("Jump") and is_on_wall_only() and direction != 0:
 		wall_pushing = true
@@ -67,11 +60,17 @@ func _physics_process(delta):
 		await get_tree().create_timer(0.05).timeout # This stinks! But it will work for now
 		wall_pushing = false
 		print(WALL_VELOCITY)
-		
+	
 	if is_on_floor():
 		WALL_VELOCITY = -900
 		was_wall_jumping = false
-		
+	
+	# Quits game
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().quit()
+	
+	#if $RayRight.is_colliding() or $RayLeft.is_colliding():
+	
 	# - ANIMATIONS - #
 	
 	if (velocity.x > 1 || velocity.x < -1) and velocity.y == 0 and not was_wall_jumping:
@@ -88,10 +87,6 @@ func _physics_process(delta):
 		$Sprite2D.flip_h = false
 	if velocity.x < 0:
 		$Sprite2D.flip_h = true
-		
-	if $RayRight.is_colliding():
-		print("right collision")
-	if $RayLeft.is_colliding():
-		print("left collision")
-		
+	
 	move_and_slide()
+
