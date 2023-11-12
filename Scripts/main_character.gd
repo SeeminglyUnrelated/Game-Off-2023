e757083e0a1d4b6d27f772d9690f092ab1fa8d7c
DanHakGit <danielhakimian@proton.me>
2023-11-12 14:07:58 -0500

    Dynamic Jump Height
    - Like in hollow knight.. Hold jump to jump higher, release to stop

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -19,24 +19,27 @@ func _physics_process(delta):
 	var direction = Input.get_axis("Left", "Right") # Assign values to left and right (-1 = left, 0 = still, 1 = right)
 	var is_wall_sliding = false # Refresh wall sliding
 	var touching_wall = false # Refresh wall touching
+	var touching_physics_object = false # Refresh phys. object check
 	
 	if $RayLeft.is_colliding() or $RayRight.is_colliding(): # Is player touching wall ? Uses short length rays
 		touching_wall = true
+		if $RayRight.get_collider() != null and $RayRight.get_collider().is_in_group("PhysicsObjects"):
+			touching_physics_object = true
+		if $RayLeft.get_collider() != null and $RayLeft.get_collider().is_in_group("PhysicsObjects"):
+			touching_physics_object = true
 
-	if not is_on_floor(): # Applies gravity
-		velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta # Applies gravity
 	else: # Resets values upon landing
 		WALL_VELOCITY = -900
 		was_wall_jumping = false
 
 	if direction and not wall_pushing:
 		velocity.x = direction * SPEED # Get input direction and increase velocity accordingly, used for x-axis movement
-		
 	else: 
 		velocity.x = move_toward(velocity.x, 0, SPEED) # Slow down after movement key released
 	
 	if direction and velocity.y > 0 and touching_wall: # Allows player to wall slide
-		velocity.y += WALL_GRAVITY
 		velocity.y = min(velocity.y, WALL_GRAVITY) # Slows descent while wall sliding
 		$Sprite2D.animation = "Sliding"
 		is_wall_sliding = true # Used to interupt animations
@@ -58,17 +61,21 @@ func _physics_process(delta):
 		if is_on_floor(): # Allows player to jump
 			velocity.y = JUMP_VELOCITY
 	
+	if Input.is_action_just_released("Jump") and velocity.y < 0 and not was_wall_jumping:
+		velocity.y = move_toward(velocity.y, 0, -velocity.y * 0.5)
+	
 	if Input.is_action_pressed("Down"): # Allows player to drop through one way platforms
 		if $RayDown.get_collider() != null and $RayDown.get_collider().is_in_group("OneWayCollisions") and direction == 0: # Player needs to be still in order to avoid animation slide bug
 			position.y += 1
 	
 	for i in get_slide_collision_count(): # Allows for pushing of physics objects
 		var collisions = get_slide_collision(i)
-		if Input.is_action_pressed("Interact") and $RayRight.get_collider() != null and $RayRight.get_collider().is_in_group("PhysicsObjects") and collisions.get_collider().is_in_group("PhysicsObjects"):
+		if Input.is_action_pressed("Interact") and touching_physics_object and collisions.get_collider().is_in_group("PhysicsObjects"):
 			collisions.get_collider().apply_central_impulse(-collisions.get_normal() * PUSH_FORCE)
-		if Input.is_action_pressed("Interact") and $RayLeft.get_collider() != null and $RayLeft.get_collider().is_in_group("PhysicsObjects") and collisions.get_collider().is_in_group("PhysicsObjects"):
-			collisions.get_collider().apply_central_impulse(-collisions.get_normal() * PUSH_FORCE)
-	
+
+	if velocity.y > 10000: # For debugging, gets player unstuck
+		velocity.y = JUMP_VELOCITY
+
 	if Input.is_action_just_pressed("Quit"): # Quits game
 		get_tree().quit()
 	
